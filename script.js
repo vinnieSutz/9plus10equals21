a9dbcefa1873a62bf00d0cfc72314e6f2f67dcc9
Vinicius Sutz <125303773+vinicius-sutz@users.noreply.github.com>
2024-01-10 00:10:03 -0300

    Added functionality to the '21' button
    Button will now play the video and then close the page after an interval

--- script.js
+++ script.js
@@ -3,36 +3,47 @@ const button19 = document.createElement('button')
 const button21 = document.createElement('button')
 const buttonsDiv = document.querySelector('.buttons-div');
 
-
-
-
 video.currentTime = 1.8;
-
 
 video.addEventListener('click', function handler(e) {
     e.currentTarget.removeEventListener(e.type, handler);
     video.play();
 
-    video.addEventListener("timeupdate", () => {
-        if(video.currentTime >= 3.4) {
+    video.addEventListener('timeupdate', function handler(e) {
+        if (video.currentTime >= 3.4) {
             video.pause();
-            button19.classList.add('dom-button', 'answer-19')
+            
+            button19.classList.add('answer-button')
             button19.textContent = '19'
         
-            button21.classList.add('dom-button', 'answer-21')
+            button21.classList.add('answer-button')
             button21.textContent = '21'
         
             buttonsDiv.appendChild(button19)
             buttonsDiv.appendChild(button21)
+            e.currentTarget.removeEventListener(e.type, handler);
         }
     });
-    
+
     // time tracker :3
     video.ontimeupdate = () => {
         document.querySelector('p').innerText = video.currentTime;
     }
 });
 
-
+button19.addEventListener('click', () => {
+    
+})
+button21.addEventListener('click', () => {
+    video.play();
+    video.addEventListener('timeupdate', () => {
+        if (video.ended === true) {
+            setTimeout(closePage, 200)
+            function closePage() {
+                window.close();
+            }
+        }
+    })
+})
 
 
